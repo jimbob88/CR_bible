d59664f42f758bfbe45e157c1bb9678d6aa75d4d
James Blackburn <blackburnfjames@gmail.com>
2022-01-14 15:38:41 +0000

    Removes PSA, GLO, FRT. Dumps to JSON

--- main.py
+++ main.py
@@ -3,35 +3,37 @@ import xml.etree.ElementTree as ET
 import os
 import bs4
 import re
+import json
 
 bible_html = './engwebpb_html/' # CHANGE THIS TO CHANGE YOUR HTML BIBLE
 
 tree = ET.parse(os.path.join(bible_html, 'engwebpb-VernacularParms.xml'))
 root = tree.getroot()
 
-
 conversion_table = {scripture.attrib['ubsAbbreviation']: scripture.text for scripture in root.findall('scriptureBook')}
+ignore = ['PSA', 'GLO', 'FRT']
 regex = (
     r'('+
-    '|'.join(conversion_table.keys())+
+    '|'.join([key for key in conversion_table.keys() if key not in ignore])+
     r')([\d]+)\.htm'
 )
 res = [os.path.join(bible_html, f) for f in os.listdir(bible_html) if re.search(regex, f)]
 dataset = {
-    conv: {} for conv in conversion_table.values()
+    conv: {} for ubs, conv in conversion_table.items() if ubs not in ignore
 }
 print(regex)
 for htm in res:
-    if os.path.split(htm)[1][0:3] == 'PSA':
-        next
-    else:
-        print(htm)
-        soup = bs4.BeautifulSoup(open(htm, 'r', encoding='utf-8').read(), "lxml")
-        # print(soup.find_all('span', {'class': 'verse'}))
-        p_verses = []
-        for paragraph in soup.find_all('div', {'class': 'p'}):
-            p_verses.append([int(v.get('id')[1:]) for v in paragraph.find_all('span', {'class': 'verse'})])
+    print(htm)
+    soup = bs4.BeautifulSoup(open(htm, 'r', encoding='utf-8').read(), "lxml")
+    # print(soup.find_all('span', {'class': 'verse'}))
+    p_verses = []
+    for paragraph in soup.find_all('div', {'class': 'p'}):
+        verse_numbers = [int(v.get('id')[1:]) for v in paragraph.find_all('span', {'class': 'verse'})]
+        if len(verse_numbers) > 1:
+            p_verses.append(verse_numbers)
+            
+    if len(p_verses) > 1:
         dataset[conversion_table[re.search(regex, htm).group(1)]][re.search(regex, htm).group(2)] = p_verses    
 
 print(dataset)
-        
+json.dump(dataset, open('dataset.json', 'w', encoding='utf-8'))
