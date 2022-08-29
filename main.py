3242c3157395e64979e6b26420ce78fcc2821d11
James Blackburn <blackburnfjames@gmail.com>
2022-08-29 13:12:27 +0100

    Improved usability / pythonic nature
    The original system was rather badly put together, this version is much cleaner and easier to understand. The code is clearly separated into each section and the use of pathlib expands the usability massively

--- main.py
+++ main.py
@@ -1,54 +1,98 @@
-import xml.etree.ElementTree as ET
-import os
-import bs4
-import re
-import json
 import argparse
+import json
+import re
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+from pathlib import Path
+from typing import List, Dict
 
-parser = argparse.ArgumentParser(description = "A software for getting verse positions by paragraph")
-parser.add_argument('--folder', '-f', required=True, help="choose the folder with the bible dataset")
-parser.add_argument('--xmlname', '-x', required=True, help="select the name of the xml file (don't pass a path)")
-args = parser.parse_args()
+import bs4
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description="A software for getting verse positions by paragraph")
+    parser.add_argument('--folder', '-f', required=True, help="choose the folder with the bible dataset")
+    parser.add_argument('--xmlname', '-x', required=True, help="select the name of the xml file 'vernacularParms'")
+    parser.add_argument('--out', '-o', default='dataset.json', help="the name of the output json")
 
-bible_html = args.folder  # CHANGE THIS TO CHANGE YOUR HTML BIBLE
+    return parser.parse_args()
 
-tree = ET.parse(
-    os.path.join(bible_html, args.xmlname)
-)  # CHANGE TO THE NAME OF YOUR BIBLE
-root = tree.getroot()
 
-conversion_table = {
-    scripture.attrib["ubsAbbreviation"]: scripture.text
-    for scripture in root.findall("scriptureBook")
-}
-ignore = ["PSA", "GLO", "FRT"]
-regex = (
-    r"("
-    + "|".join([key for key in conversion_table.keys() if key not in ignore])
-    + r")([\d]+)\.htm"
-)
-res = [
-    os.path.join(bible_html, f) for f in os.listdir(bible_html) if re.search(regex, f)
-]
-dataset = {conv: {} for ubs, conv in conversion_table.items() if ubs not in ignore}
-for htm in res:
-    print(htm)
-    soup = bs4.BeautifulSoup(open(htm, "r", encoding="utf-8").read(), "lxml")
+def abbreviations(vernacular_parms_xml: Path) -> Dict[str, str]:
+    """Given a vernacular parms xml, it discovers all the abbreviations used
 
-    p_verses = []
-    for paragraph in soup.find_all("div", {"class": "p"}):
-        verse_numbers = [
-            int(v.get("id")[1:]) for v in paragraph.find_all("span", {"class": "verse"})
-        ]
-        if len(verse_numbers) > 1:
-            p_verses.append(verse_numbers)
+    :rtype: dict[str, str]
+    :return i.e. {'PSA': 'Psalms'}
+    """
+    tree = ET.parse(vernacular_parms_xml)
+    root = tree.getroot()
+    return {
+        scripture.attrib["ubsAbbreviation"]: scripture.text
+        for scripture in root.findall("scriptureBook")
+    }
 
-    if len(p_verses) > 1:
-        dataset[conversion_table[re.search(regex, htm).group(1)]][
-            re.search(regex, htm).group(2)
-        ] = p_verses
 
-print(dataset)
-json.dump(dataset, open("dataset.json", "w", encoding="utf-8"))
+def htm_filename_regex(abbreviations: List[str], ignore: List[str]):
+    """Returns a list of htm file names
+    
+    :argument abbreviations: i.e. ['PSA', 'GLO', 'FRT']
+    :return: A regex, group 1 is the abbreviation, group 2 is the chapter number
+    """
+
+    return (
+            r"("
+            + "|".join([key for key in abbreviations if key not in ignore])
+            + r")([\d]+)\.htm"
+    )
+
+
+def filter_folder(folder: Path, regex: str) -> List[Path]:
+    """Given a regex, return all file names which match said regex"""
+    return [element for element in folder.iterdir() if element.is_file() and re.search(regex, element.name)]
+
+
+def paragraphs(soup: bs4.BeautifulSoup) -> List[bs4.Tag]:
+    return soup.find_all("div", {"class": "p"})
+
+
+def verses(element: bs4.Tag):
+    return element.find_all("span", {"class": "verse"})
+
+
+def remove_empty(lst: List):
+    return [item for item in lst if item]
+
+
+def verse_numbers(paragraph: bs4.Tag):
+    return [int(verse.get("id")[1:]) for verse in verses(paragraph)]
+
+
+def paragraph_carriage_returns(soup: bs4.BeautifulSoup):
+    """Returns carriage returns that appear on each verse"""
+    paragraph_verses = [
+        [int(verse.get("id")[1:]) for verse in verses(paragraph)] for paragraph in paragraphs(soup)
+    ]
+    return remove_empty(paragraph_verses)
+
+
+if __name__ == '__main__':
+    args = get_args()
+    folder = Path(args.folder)
+
+    conversion_table = abbreviations(folder / args.xmlname)
+    filename_regex = htm_filename_regex(list(conversion_table.keys()), [])
+
+    dataset = defaultdict(dict)
+    for file in filter_folder(folder, filename_regex):
+        book_abbreviation = re.search(filename_regex, file.name).group(1)
+        book_name = conversion_table[book_abbreviation]
+        chapter = re.search(filename_regex, file.name).group(2)
+
+        soup = bs4.BeautifulSoup(file.read_text('utf-8'), 'lxml')
+        p_verses = paragraph_carriage_returns(soup)
+        if p_verses:
+            dataset[book_name][chapter] = p_verses
+
+    print(dataset)
+
+    json.dump(dataset, Path(args.out).open('w', encoding='utf-8'))
