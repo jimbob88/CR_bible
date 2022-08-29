d073eb490cfc92c4bea1b032d91834a21dbfff6f
James Blackburn <blackburnfjames@gmail.com>
2022-08-29 13:23:24 +0100

    Changed outerspace to local

--- main.py
+++ main.py
@@ -10,10 +10,26 @@ import bs4
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="A software for getting verse positions by paragraph")
-    parser.add_argument('--folder', '-f', required=True, help="choose the folder with the bible dataset")
-    parser.add_argument('--xmlname', '-x', required=True, help="select the name of the xml file 'vernacularParms'")
-    parser.add_argument('--out', '-o', default='dataset.json', help="the name of the output json")
+    parser = argparse.ArgumentParser(
+        description="A software for getting verse positions by paragraph")
+    parser.add_argument('--folder',
+                        '-f',
+                        required=True,
+                        help="choose the folder with the bible dataset")
+    parser.add_argument(
+        '--xmlname',
+        '-x',
+        required=True,
+        help="select the name of the xml file 'vernacularParms'")
+    parser.add_argument('--out',
+                        '-o',
+                        default='dataset.json',
+                        help="the name of the output json")
+    parser.add_argument('-i',
+                        '--ignore',
+                        nargs='+',
+                        help='Ignore list',
+                        default=['PSA', 'GLO', 'FRT'])
 
     return parser.parse_args()
 
@@ -39,16 +55,17 @@ def htm_filename_regex(abbreviations: List[str], ignore: List[str]):
     :return: A regex, group 1 is the abbreviation, group 2 is the chapter number
     """
 
-    return (
-            r"("
-            + "|".join([key for key in abbreviations if key not in ignore])
-            + r")([\d]+)\.htm"
-    )
+    return (r"(" +
+            "|".join([key for key in abbreviations if key not in ignore]) +
+            r")([\d]+)\.htm")
 
 
 def filter_folder(folder: Path, regex: str) -> List[Path]:
     """Given a regex, return all file names which match said regex"""
-    return [element for element in folder.iterdir() if element.is_file() and re.search(regex, element.name)]
+    return [
+        element for element in folder.iterdir()
+        if element.is_file() and re.search(regex, element.name)
+    ]
 
 
 def paragraphs(soup: bs4.BeautifulSoup) -> List[bs4.Tag]:
@@ -69,18 +86,18 @@ def verse_numbers(paragraph: bs4.Tag):
 
 def paragraph_carriage_returns(soup: bs4.BeautifulSoup):
     """Returns carriage returns that appear on each verse"""
-    paragraph_verses = [
-        [int(verse.get("id")[1:]) for verse in verses(paragraph)] for paragraph in paragraphs(soup)
-    ]
+    paragraph_verses = [[
+        int(verse.get("id")[1:]) for verse in verses(paragraph)
+    ] for paragraph in paragraphs(soup)]
     return remove_empty(paragraph_verses)
 
 
-if __name__ == '__main__':
+def main():
     args = get_args()
     folder = Path(args.folder)
 
     conversion_table = abbreviations(folder / args.xmlname)
-    filename_regex = htm_filename_regex(list(conversion_table.keys()), [])
+    filename_regex = htm_filename_regex(list(conversion_table.keys()), args.ignore)
 
     dataset = defaultdict(dict)
     for file in filter_folder(folder, filename_regex):
@@ -96,3 +113,7 @@ if __name__ == '__main__':
     print(dataset)
 
     json.dump(dataset, Path(args.out).open('w', encoding='utf-8'))
+
+
+if __name__ == '__main__':
+    main()
